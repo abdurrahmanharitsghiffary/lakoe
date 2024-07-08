c352f53a31fea40caf187d0554762e4088e993c8
aristu09 <namikazeuzumaki43@gmail.com>
2024-07-08 16:38:08 +0700

    chore:slicing heading card product

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,10 +2,10 @@
 module.exports = {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
   ],
   prefix: "",
   theme: {
@@ -17,7 +17,11 @@ module.exports = {
       },
     },
     extend: {
+      backgroundColor: {
+        "btn-primary": "#0086b4",
+      },
       colors: {
+        "txt-secondary": "#0086b4",
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
         ring: "hsl(var(--ring))",
@@ -74,4 +78,4 @@ module.exports = {
     },
   },
   plugins: [require("tailwindcss-animate")],
-}
+};
