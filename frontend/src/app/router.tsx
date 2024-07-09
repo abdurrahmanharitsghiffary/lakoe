2db46b959dee3b65d90f2f59bff540d8789809d0
dangdang2121 <haffae124@gmail.com>
2024-07-09 10:03:26 +0700

    chore: add Typography and edit navigation components

--- frontend/src/app/router.tsx
+++ frontend/src/app/router.tsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import HomePage from "./pages/home";
 import App from "./app";
 import { ProductsPage } from "./pages/products";
+import Typography from "@/components/ui/typography";
 
 export const router = createBrowserRouter([
   {
@@ -12,6 +13,14 @@ export const router = createBrowserRouter([
       {
         path: "products",
         element: <ProductsPage />,
+      },
+      {
+        path: "*",
+        element: (
+          <div className="h-[100dvh] flex justify-center items-center">
+            <Typography variant="h3">Page not found.</Typography>
+          </div>
+        ),
       },
     ],
   },
