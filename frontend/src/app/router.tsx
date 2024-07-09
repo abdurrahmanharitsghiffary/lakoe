95f6c39f73eb0d6b472e3255281021781185ec26
aristu09 <namikazeuzumaki43@gmail.com>
2024-07-09 10:00:12 +0700

    chore:add-new-product

--- frontend/src/app/router.tsx
+++ frontend/src/app/router.tsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import HomePage from "./pages/home";
 import App from "./app";
 import { ProductsPage } from "./pages/products";
+import { FormProduct } from "@/features/products/form-product";
 
 export const router = createBrowserRouter([
   {
@@ -12,6 +13,10 @@ export const router = createBrowserRouter([
       {
         path: "products",
         element: <ProductsPage />,
+      },
+      {
+        path: "products/create",
+        element: <FormProduct />,
       },
     ],
   },
