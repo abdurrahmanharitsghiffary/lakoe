f32df51b9f55781e48d5cd5a410ac41e98281a91
ramakusuma10 <ramakusuma098@gmail.com>
2024-07-26 00:40:08 +0700

    add cart component

--- frontend/src/components/cart/cartlist.tsx
+++ frontend/src/components/cart/cartlist.tsx
@@ -1,4 +1,4 @@
-import { useState,useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { Store } from './store';
 import { Card } from "../ui/card";
 import { FaRegCircleCheck } from "react-icons/fa6";
@@ -9,193 +9,238 @@ import { Input } from "../ui/input";
 import { Label } from "../ui/label";
 import { formatRupiah } from "@/utils/format-currency";
 import { CheckoutDialogVoucher } from '../dialog/checkout-dilalog-voucher';
-
-interface ProductType {
-  id: number;
-  name: string;
-  image: string;
-  price: number;
-  count: number;
-  variant: string;
-  checked: boolean;
-
-}
-
-interface CartType {
-  id: number;
-  name: string;
-  products: ProductType[];
-  checked: boolean;
-  disabled: boolean;
-}
+import { SKUAttribute,StoreType} from '@/types/cart';
+import { useCartCollection } from '@/features/cart/api/get-cart';
+import { deleteProduct,deleteCart } from '@/features/cart/api/delete-cart';
 
 export function CartList() {
+  const cartId = '034a015c-bd4b-41cc-82e3-0fc7d57409fb';
   const [totalPrice, setTotalPrice] = useState(0);
   const [totalCount, setTotalCount] = useState(0);
   const [isVoucherOpen, setIsVoucherOpen] = useState(false);
-  const [carts, setCarts] = useState<CartType[]>([
-    {
-      id: 1,
-      name: "Toko A",
-      products: [
-        {
-          id: 1,
-          name: "Sepatu Mantap",
-          image: "assets/sepatuputih.jpeg",
-          variant: "Putih",
-          price: 210000,
-          count: 1,
-          checked: false,
-        }, {
-          id: 2,
-          name: "Sepatu Mantap",
-          variant: "Merah",
-          image: "assets/sepatuputih.jpeg",
-          price: 310000,
-          count: 1,
-          checked: false,
-        }
-      ],
-      checked: false,
-      disabled: false,
-    }, {
+  const [note, setNote] = useState('');
+  const [noteLength, setNoteLength] = useState(0);
+  const [stores, setStores] = useState<StoreType[]>([]);
+  const {data: cartData, error, isLoading } = useCartCollection(cartId);
 
-      id: 2,
-      name: "Toko B",
-      products: [
-        {
-          id: 1,
-          name: "Sepatu Mantap",
-          image: "assets/sepatuputih.jpeg",
-          variant: "Putih",
-          price: 210000,
-          count: 2,
+ 
+  useEffect(() => {
+    if(cartData){
+    const fetchedStores:StoreType[] = cartData.map((cart: any) => ({
+        id: cart.store.id,
+        name:cart.store.name,
+        checked: false,
+        disabled: false,
+        cartId: cart.id,
+        products: cart.cartItems.map((item: any) => ({
+          id: item.sku.product.id,
+          name: item.sku.product.name,
+          image: item.sku.image,
+          count: item.qty,
+          skuId: item.sku.id,
           checked: false,
-        }, {
-          id: 2,
-          name: "Sepatu Mantap",
-          variant: "Merah",
-          image: "assets/sepatuputih.jpeg",
-          price: 310000,
-          count: 3,
-          checked: false,
-        }
-      ],
-      checked: false,
-      disabled: false
-    },
-  ]);
+          price: parseFloat(item.sku.price),
+          attributes: item.sku.skuAttributes.map((attr: any) => ({
+            value: attr.value,
+            attribute: attr.attribute,
+        })),
+      })),
+    }));
+    setStores(fetchedStores);
+  }
+  }, [cartData]);
 
   // const setAllChecked = useState(false);
-  
+
   // useEffect(() => {
   //   const allStoresChecked = carts.every(cart => cart.checked);
   //   setAllChecked(allStoresChecked);
   // }, [carts]);
 
-    useEffect(() => {
-    const total = carts.reduce((sum, cart) => 
-      sum + cart.products.reduce((productSum, product) => 
+  useEffect(() => {
+    const total = stores.reduce((sum, cart) =>
+      sum + cart.products.reduce((productSum, product) =>
         product.checked ? productSum + product.price * product.count : productSum, 0), 0);
     setTotalPrice(total);
-  }, [carts]);
+  }, [stores]);
 
   useEffect(() => {
-    const total = carts.reduce((sum, cart) =>
+    const total = stores.reduce((sum, cart) =>
       sum + cart.products.reduce((productSum, product) =>
         product.checked ? productSum + product.count : productSum, 0), 0);
     setTotalCount(total);
-  }, [carts]);
+  }, [stores]);
 
-  const handleIncrement = (cartId: number, productId: number) => {
-    setCarts(prevCarts =>
-      prevCarts.map(cart =>
-        cart.id === cartId ?
+  const handleIncrement = (storeId: number, productId: number, attributes: SKUAttribute[]) => {
+    setStores(prevStores =>
+      prevStores.map(store =>
+        store.id === storeId ?
           {
-            ...cart,
-            products: cart.products.map(product => product.id === productId ? { ...product, count: product.count + 1,checked:true } : product
+            ...store,
+            products: store.products.map(product => product.id === productId && JSON.stringify(product.attributes) === JSON.stringify(attributes)
+              ? { ...product, count: product.count + 1, checked: true }
+              : product
             ),
-            checked:cart.products.some(product => product.checked || product.id === productId)
+            checked: store.products.some(product => product.checked || product.id === productId)
           }
-          : cart
+          : store
       )
     );
   };
 
-  const handleDecrement = (cartId: number, productId: number) => {
-    setCarts(prevCarts =>
-      prevCarts.map(cart =>
-        cart.id === cartId ?
+  const handleDecrement = (storeId: number, productId: number, attributes: SKUAttribute[]) => {
+    setStores(prevStores =>
+      prevStores.map(store =>
+        store.id === storeId ?
           {
-            ...cart,
-            products: cart.products.map(product => product.id === productId ? { ...product, count: Math.max(product.count - 1, 1) } : product
+            ...store,
+            products: store.products.map(product => product.id === productId && JSON.stringify(product.attributes) === JSON.stringify(attributes) && product.count > 1
+              ? { ...product, count: product.count - 1 }
+              : product
             ),
           }
-          : cart
+          : store
       )
     );
   };
 
-  const handleStoreCheck = (cartId: number) => {
-    setCarts(prevCarts => {
-      const newCarts = prevCarts.map(cart => cart.id === cartId ?
-        {
-          ...cart,
-          checked: !cart.checked,
-          products: cart.products.map(product => ({ ...product, checked: !cart.checked })),
-        }
-        : { ...cart, disable: !prevCarts.find(c => c.id === cartId)!.checked }
-      );
-      const anyChecked = newCarts.some(cart => cart.checked);
-      return newCarts.map(cart => ({
-        ...cart,
-        disabled: anyChecked && !cart.checked,
-      }));
-    });
-
-  };
-
-  const handleProductCheck = (cartId: number, productId: number) => {
-    setCarts(prevCarts => {
-      const newCarts = prevCarts.map(cart =>
-        cart.id === cartId
+  const handleStoreCheck = (storeId: number) => {
+    setStores(prevStores => {
+      const newStores = prevStores.map(store =>
+        store.id === storeId
           ? {
-            ...cart,
-            products: cart.products.map(product => product.id === productId ? { ...product, checked: !product.checked } : product),
-            checked: cart.products.some(product => product.id === productId ? !product.checked : product.checked)
-          }
-          : cart
-      )
-      const anyChecked = newCarts.some(cart => cart.checked || cart.products.some(product => product.checked));
-      return newCarts.map(cart => ({
-        ...cart,
-        disabled: anyChecked && !cart.checked && !cart.products.some(product => product.checked),
+              ...store,
+              checked: !store.checked,
+              products: store.products.map(product => ({ ...product, checked: !store.checked })),
+            }
+          : store
+      );
+
+      const anyChecked = newStores.some(store => store.checked || store.products.some(product => product.checked));
+      return newStores.map(store => ({
+        ...store,
+        disabled: anyChecked && !store.checked && !store.products.some(product => product.checked),
       }));
     });
+  };
+
+  const handleProductCheck = (storeId: number, productId: number, attributes: SKUAttribute[]) => {
+    setStores(prevStores => {
+      const newStores = prevStores.map(store => {
+        if (store.id === storeId) {
+          const newProducts = store.products.map(product =>
+            product.id === productId && JSON.stringify(product.attributes) === JSON.stringify(attributes)
+              ? { ...product, checked: !product.checked }
+              : product
+          );
+
+          const allProductsChecked = newProducts.every(product => product.checked);
+
+          return {
+            ...store,
+            checked: allProductsChecked,
+            products: newProducts,
+          };
+        }
+        return store;
+      });
+
+      const anyChecked = newStores.some(store => store.products.some(product => product.checked));
+      return newStores.map(store => ({
+        ...store,
+        disabled: anyChecked && store.id !== storeId,
+      }));
+    });
+  };
+  
+  const handleNoteChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newNote = e.target.value;
+    if (newNote.length <= 150) {
+      setNote(newNote);
+      setNoteLength(newNote.length);
+    }
+  };
+
+  const handleCheckout = () => {
+    const checkedItems = stores.flatMap(store =>
+      store.products
+        .filter(product => product.checked)
+        .map(product => ({
+          name: product.name,
+          count: product.count,
+          totalprice: product.count * product.price,
+        }))
+    );
+
+
+    const totalProducts = checkedItems.reduce((acc, item) => acc + item.count, 0);
+    const totalPrice = checkedItems.reduce((acc, item) => acc + item.totalprice, 0);
+
+    const checkoutData = {
+      products: checkedItems,
+      totalProducts,
+      totalPrice,
+      note,
+    };
+
+    console.log(checkoutData);
 
   };
+
+  const HandleDeleteProduct = async (cartId:string, skuId:number) =>{
+    try {
+      await deleteProduct(cartId, skuId);
+      setStores(prevStores =>{
+        const updatedStores = prevStores.map(store =>({
+          ...store,
+          products: store.products.filter(product => product.skuId !== skuId),
+
+        }));
+        
+        
+        // Remove cart if no products are left
+        const cartIdsToDelete = updatedStores
+          .filter(store => store.products.length === 0)
+          .map(store => store.cartId);
+
+        // Remove carts
+        cartIdsToDelete.forEach(async (id) => {
+          try {
+            await deleteCart(id);
+            console.log(`Cart ${id} deleted successfully`);
+          } catch (error) {
+            console.error(`Failed to delete cart ${id}:`, error);
+          }
+        });
+  
+        return updatedStores.filter(store => store.products.length > 0);
+      });
+      
+    } catch (error) {
+      console.error('Error deleting product:', error);
+    }
+  }
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>Error loading data</div>;
 
   return (
     <div className="flex flex-col w-full px-6">
       <h1 className="text-[30px] mt-10 my-6 ml-4">Keranjang</h1>
       <div className='flex flex-row'>
-        <div className='flex flex-col'>
-          {carts.map(cart => (
-            <Store
-              key={cart.id}
-              storeName={cart.name}
-              products={cart.products}
-              onIncrement={(productId) => handleIncrement(cart.id, productId)}
-              onDecrement={(productId) => handleDecrement(cart.id, productId)}
-              checked={cart.checked}
-              onStoreCheck={() => handleStoreCheck(cart.id)}
-              onProductCheck={(productId) => handleProductCheck(cart.id, productId)}
-              disabled={cart.disabled}
-            />
-          ))}`
+        <div className='flex flex-col w-full'>
+          <Store
+            stores={stores}
+            setStores={setStores}
+            onIncrement={handleIncrement}
+            onDecrement={handleDecrement}
+            onStoreCheck={handleStoreCheck}
+            onProductCheck={handleProductCheck}
+            onProductDelete={HandleDeleteProduct}
+          />
         </div>
         <div className="basis-1/4">
-          <Card className="mx-2 rounded-xl border-blue-500 mt-6">
+          <Card className="rounded-xl border-blue-500 mt-6 ml-[-8px] mr-2 ">
             <Button className="border-b-2 bg-transparent text-black hover:bg-white rounded-b-none w-full flex justify-between py-8" onClick={() => setIsVoucherOpen(true)}>
               <div className="flex flex-row w-100 ">
                 <div className="flex flex-row w-full items-center gap-4">
@@ -220,24 +265,27 @@ export function CartList() {
               </div>
             </div>
           </Card>
-          <Card className='rounded-xl mt-6 mx-2 border-blue-500'>
+          <Card className='rounded-xl mt-6 ml-[-8px] mr-2  border-blue-500'>
             <div className="flex flex-col px-5 text-lg py-7 gap-1">
               <Label className="text-xl font-bold ">Catatan</Label>
               <div className='flex flex-col'>
                 <Input
                   placeholder="Tulis Catatan/intruksi pesananmu"
                   className="text-lg  rounded-[10px] py-6 w-full"
+                  value={note}
+                  onChange={handleNoteChange}
+                  maxLength={150}
                 />
-                <span className="text-lg text-end ">0/150</span>
+                <span className="text-lg text-end ">{noteLength}/150</span>
               </div>
             </div>
           </Card>
-          <Button className="bg-blue-500 w-[395px] text-xl py-6 my-4 mt-5 ml-2 drop-shadow-lg hover:bg-blue-400 space-x-3 " >
+          <Button className="bg-blue-500 w-[403px] text-xl py-6 my-4 mt-5 mr-2 ml-[-7px] drop-shadow-lg hover:bg-blue-400 space-x-3 " onClick={handleCheckout} >
             <span><FaRegCircleCheck className="h-6 w-6" /></span>
             <span>Checkout({totalCount})</span>
 
           </Button>
-          <CheckoutDialogVoucher onOpen={setIsVoucherOpen} isOpen={isVoucherOpen}/>
+          <CheckoutDialogVoucher onOpen={setIsVoucherOpen} isOpen={isVoucherOpen} />
         </div>
       </div>
     </div>
--- frontend/src/components/cart/store.tsx
+++ frontend/src/components/cart/store.tsx
@@ -9,44 +9,33 @@ import {
 import { Card } from "../ui/card";
 import { Checkbox } from "../ui/checkbox";
 import { FaStore } from "react-icons/fa";
+import { StoreType,SKUAttribute } from "@/types/cart";
 
-
-interface ProductType {
-  id: number;
-  name: string;
-  image: string;
-  count: number;
-  variant: string;
-  price: number;
-  checked: boolean;
-
-}
-
-interface ProductProps {
-  storeName: string;
-  products: ProductType[];
-  onIncrement: (productId: number) => void;
-  onDecrement: (productid: number) => void;
-  checked: boolean;
-  onStoreCheck: () => void;
-  onProductCheck: (productId:number) => void;
-  disabled: boolean;
+interface StoreProps {
+  stores: StoreType[];
+  setStores: React.Dispatch<React.SetStateAction<StoreType[]>>;
+  onIncrement: (storeId:number, productId: number, attributes: SKUAttribute[]) => void;
+  onDecrement: (storeId:number, productId: number, attributes: SKUAttribute[]) => void;
+  onStoreCheck: (storeId:number) => void;
+  onProductCheck: (storeId:number,productId:number, attributes: SKUAttribute[]) => void;
+  onProductDelete: (cartId:string,skuId:number) => void;
 }
  
-export function Store({ storeName, products, onIncrement, onDecrement,checked,onStoreCheck,onProductCheck,disabled}: ProductProps) {
+export function Store({ stores,onIncrement, onDecrement,onStoreCheck,onProductCheck,onProductDelete}: StoreProps) {
   return (
     <div className="flex flex-row w-full mt-6">
       <div className="basis-3/4">
-        <Card className="mx-4 rounded-xl">
-          <Table className="bg-white text-xl w-[1190px] mt-2">
+        {stores.map(store =>(
+        <Card className="ml-3 mb-6 rounded-xl">
+          <Table className="bg-white text-xl w-[1190px] mt-2" key={store.id}>
             <TableHeader>
               <TableRow className="text-[15px] hover:bg-white">
                 <TableHead className='flex flex-row items-center space-x-2'>
                   <span className="px-1">
-                      <Checkbox className="h-[25px] w-[25px] border-blue-500 data-[state=checked]:bg-blue-500" checked={checked} onCheckedChange={onStoreCheck} disabled={disabled}/>
+                      <Checkbox className="h-[25px] w-[25px] border-blue-500 data-[state=checked]:bg-blue-500" checked={store.checked} onCheckedChange={()=>onStoreCheck(store.id)} disabled={store.disabled}/>
                   </span>
                   <span className="px-1 pb-2"><FaStore color="blue" className="h-6 w-6" /></span>
-                  <span className='pb-3 text-lg'>{storeName}</span>
+                  <span className='pb-3 text-lg'>{store.name}</span>
                 </TableHead>
                 <TableHead className="pt-3 pb-4"></TableHead>
                 <TableHead className="pt-3 pb-4"></TableHead>
@@ -54,19 +43,17 @@ export function Store({ storeName, products, onIncrement, onDecrement,checked,on
             </TableHeader>
             <TableBody>
 
-              {products.map(product => (
+              {store.products.map(product => (
                 <ProductTable
-                  key={product.id}
-                  image={product.image}
-                  productName={product.name}
-                  count={product.count}
-                  variant={product.variant}
-                  price={product.price}
-                  onIncrement={() => onIncrement(product.id)}
-                  onDecrement={() => onDecrement(product.id)}
-                  checked={product.checked}
-                  onCheck={()=> onProductCheck(product.id)}
-                  disabled={disabled}
+                  key={`${product.id}-${JSON.stringify(product.attributes)}`}
+                  storeId={store.id}
+                  product={product}
+                  onIncrement={onIncrement}
+                  onDecrement={onDecrement}
+                  onProductCheck={onProductCheck}
+                  storeDisabled={store.disabled}
+                  onProductDelete={onProductDelete}
+                  cartId={store.cartId}
                 />
               ))}
             </TableBody>
@@ -74,6 +61,7 @@ export function Store({ storeName, products, onIncrement, onDecrement,checked,on
           <div className="flex flex-row px-4 border-t-2 py-7">
           </div>
         </Card>
+        ))}
       </div>
     </div>
   )
