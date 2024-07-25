fb869ae7192662a924e03d8865e067ae850e610f
aristu09 <namikazeuzumaki43@gmail.com>
2024-07-25 21:50:30 +0700

    feat:add-courier

--- frontend/src/hooks/use-string.ts
+++
@@ -1,5 +0,0 @@
-export const convertLatLngToString = (latlng: L.LatLng): string => {
-  const lat = latlng.lat.toFixed(5); // Membatasi angka desimal hingga 5 digit
-  const lng = latlng.lng.toFixed(5); // Membatasi angka desimal hingga 5 digit
-  return `Latitude: ${lat}, Longitude: ${lng}`;
-};
